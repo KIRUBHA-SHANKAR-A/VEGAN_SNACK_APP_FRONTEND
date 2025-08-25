f2f71204f6f8decaf277fc6279f70cd40f951787
KIRUBHA-SHANKAR-A <727723eucs106@skcet.ac.in>
2025-08-25 09:00:34 +0530

    Update AddVeganSnack.jsx

--- src/components/AddVeganSnack.jsx
+++ src/components/AddVeganSnack.jsx
@@ -109,7 +109,7 @@ const [form, setForm] = useState({
           vendorId: vendorId,
         };
 
-        const res = await fetch('https://springapp-production.up.railway.app/snacks', {
+        const res = await fetch('https://vegansnackappbackend-production.up.railway.app/snacks', {
           method: 'POST',
           headers: { 
             'Content-Type': 'application/json',
